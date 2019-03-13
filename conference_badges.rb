8580ca9baf4a4d590586c63dc27891d38bb2b1ba
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-13 01:23:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  names = []
+  array.each {|i| names << badge_maker(i)}
+  names
+end
+    
+def assign_rooms(array)
+  array.each_with_index do |value, i|
+   "Hello #{value}, you will be in room #{i + 1}"
+ end
+end
