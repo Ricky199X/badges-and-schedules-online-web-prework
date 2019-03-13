e8aa962232433a2efee73a58f4ff60450ee1fa49
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-13 01:46:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,11 +2,13 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
+
 def batch_badge_creator(array)
   names = []
   array.each {|i| names << badge_maker(i)}
   names
 end
+   
     
 def assign_rooms(array)
   array.each_with_index.collect {|value, i|
